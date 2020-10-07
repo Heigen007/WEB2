1607f0da9b298a73559068b4af8ec5aa16c05679
Heigen <grisakovr@gmail.com>
2020-10-07 19:32:48 +0600

    в
    в

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-    </div>
+      <!-- <router-link to="/">Home</router-link> | -->
     <router-view/>
   </div>
 </template>
@@ -14,6 +12,9 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
 
+}
+body{
+    background-color: #02386d;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,56 @@
 <template>
- <input type="text" id = 'but'>
+<div class = "reg">
+  <div class = "text">Регистрация</div>
+  <input type="text" id = 's' class = "input">
+  <div><button class = "but" @click="click">Войти в чат</button></div>
+</div>
+
 </template>
 
 <script>
-export default {
-  // name: 'HelloWorld',
-  // props: {
-  //   msg: String
-  // }
-}
 
-// const but = document.getElementById('but');
-// let user = {
-//     name : 'Heigen'
-// }
-// but.onclick = function(){
-//     console.log("f");
-//     let response =  fetch('http://localhost:3000/user',{
-//         method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json;charset=utf-8'
-//   },
-//   body: JSON.stringify(user)
-//     })
-// }
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    click () {
+      console.log('d')
+      const dar = document.getElementById('s')
+
+      const user = {
+        name: dar.value
+      }
+      fetch('http://localhost:3000/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+      })
+    }
+  }
+}
 
 </script>
 
 <style scoped lang="scss">
-input{
-    color: blue;
+.input{
+    border: 5px solid blue;
+    width: 200px;
+    height:50px;
+    font-size: 50px;
+    margin-top: 2%;
+    border-radius: 10%;
 }
+.text{
+  font-size: 100px;
+  color: #e28a61;
+}
+.but{
+    font-size: 50px;
+    margin-top: 3%;
+}
+
 </style>
