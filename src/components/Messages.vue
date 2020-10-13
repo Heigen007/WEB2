3fb579c89dda271772bab30d59ba6696da88a957
Heigen007 <grisakovr@gmail.com>
2020-10-13 11:52:22 +0600

    j

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -2,13 +2,13 @@
 <div class = "Messages" id = 'scroll'>
   <ul id="v-for-object" class="demo">
   <li v-for="(msg,i) in messages" :key="i">
-    <div v-if="msg.name != user" class = "MsgName">{{msg.name}}:</div>
+    <div v-if="msg.name != user" class = "MsgName1">
+      <b>{{msg.name}}</b><br>
+      <div class = "ra">{{msg.text}}</div>
+      </div>
     <div v-else class = "MsgName2">
-      {{msg.name}}:
-    </div>
-    <div v-if="msg.name != user"  class = "MsgText">{{msg.text}}</div>
-    <div v-else class = "MsgText2">
-      {{msg.text}}
+      <b>{{msg.name}}</b><br>
+      <div class = "ra">{{msg.text}}:</div>
     </div>
     </li>
 </ul>
@@ -37,31 +37,30 @@ export default {
     font-size: 30px;
     color:#3489d2;
 }
-.MsgName{
+.MsgName1{
   margin-top:10px;
   font-size: 20px;
-  color:#bf1a1a;
-  margin-left: -40px;
-}
-.MsgText{
-    margin-top:5px;
-     word-wrap: break-word;
-     width:240px;
+  color:#0d4bd6;
+  word-wrap: break-word;
+  width:200px;
+  border: 1px solid #5580a3;
+  border-radius: 10%;
 }
 .MsgName2{
   margin-top:10px;
   font-size: 20px;
-  color:#bf1a1a;
+  color:#0b8fc1;
   margin-left: 260px;
-}
-.MsgText2{
-    margin-top:5px;
-    margin-left: 300px;
-     word-wrap: break-word;
-     width:240px;
+  word-wrap: break-word;
+  border: 1px solid #5580a3;
+  border-radius: 10%;
+  width:250px;
 }
 li{
     margin-top:10px;
     list-style-type: none;
+}
+.ra{
+  margin-left: 20px;
 }
 </style>
