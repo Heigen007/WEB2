bed21c35f3a3cdaf315091ae66c8e2b007fbe4da
Heigen <grisakovr@gmail.com>
2020-10-19 18:26:35 +0600

    рш

--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -2,14 +2,28 @@
 <div class = "MessageForm" v-on:keyup.enter="send">
    <input id = "input" class = "MsgText" placeholder="Введите сообщение:">
    <img src="../assets\Sen.svg" class = "MsgBut"  @click="click">
+   <div id = "Touch" class = "down" >
+     <img src="../assets\down.svg" class = "DownBut" @click="Downclick">
+     <Stikers v-if="Hi" class = "stik"/>
+   </div>
 </div>
 
 </template>
 
 <script>
 
+import Stikers from '@/components/Stikers.vue'
+
 export default {
   name: 'MessageForm',
+  components: {
+    Stikers
+  },
+  data: function () {
+    return {
+      Hi: false
+    }
+  },
   props: {
     user: String,
     root: String
@@ -40,6 +54,16 @@ export default {
     },
     send () {
       this.click()
+    },
+    Downclick () {
+      const h = document.getElementById('Touch')
+      if (h.style.height === '240px') {
+        h.style.height = '20px'
+        this.Hi = false
+      } else {
+        h.style.height = '240px'
+        this.Hi = true
+      }
     }
   }
 }
@@ -48,19 +72,35 @@ export default {
 <style scoped lang="scss">
 .MsgText{
     color:#41cccc;
-    height: 48px;
+    height: 50px;
     width: 550px;
     float:left;
     font-size: 20px;
     outline: none;
     border:none;
+    padding: 0;
 }
 .MsgText:focus{
-      height: 45px;
+      height: 47px;
   border-bottom: 3px solid #47a3f3;
 }
 .MsgBut{
-  height:50px;
   width:50px;
+}
+.down{
+  margin-top: -4px;
+  text-align:center;
+  width: 550px;
+  margin-left: 300px;
+  height: 20px;
+  background-color: #a1d1ec;
+  // transition: height 1s;
+  align-items: center;
+}
+img{
+  width: 15px;
+}
+.stik{
+  margin-left: 210px;
 }
 </style>
--- src/components/Stikers.vue
+++ src/components/Stikers.vue
@@ -1,5 +1,14 @@
 <template>
-<div class = "Stickers">
+<div class = "Stikers">
+    <img src="../assets\svg\001-monitor.svg" class = "Stiker">
+    <img src="../assets\svg\002-smile-1.svg" class = "Stiker">
+    <img src="../assets\svg\003-diamond.svg" class = "Stiker">
+    <img src="../assets\svg\004-bar-chart.svg" class = "Stiker">
+    <img src="../assets\svg\168-padlock.svg" class = "Stiker">
+    <img src="../assets\svg\159-email.svg" class = "Stiker">
+    <img src="../assets\svg\160-chat.svg" class = "Stiker">
+    <img src="../assets\svg\166-hourglass.svg" class = "Stiker">
+    <img src="../assets\svg\147-medal.svg" class = "Stiker">
 </div>
 
 </template>
@@ -7,14 +16,17 @@
 <script>
 
 export default {
-  name: 'Stickers',
-  props: {
-    users: Array,
-    name: String
-  }
+  name: 'Stickers'
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.Stikers{
+  display: grid;
+  grid-template-columns: 50px 50px 50px;
+  grid-template-rows: 50px 50px 50px;
+}
+.Stiker{
+  width: 30px;
+}
 </style>
